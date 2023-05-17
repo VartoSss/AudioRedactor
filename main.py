5e4c7c38c9664b679d9a9223bfb50b71e257c8f0
VartoSss <ngaseneger@gmail.com>
2023-05-18 01:00:31 +0500

    manually tested functions

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from Fragment import Fragment
 from TimeLine import TimeLine
-from pydub import AudioSegment
 
 timeLine = TimeLine()
-morgen = Fragment("MORGENSHTERN_-_DULO_72947606.mp3")
+koktail = Fragment("LSP_-_Koktejjl_47965009.mp3")
+golden_fishes = Fragment("LSP_-_Zolotye_rybki_48616367.mp3")
+without_you = Fragment("Dora_LSP_-_Net_tebya_74372746.mp3")
+meteor_rain = Fragment("LSP_-_Meteoritnyjj_dozhd_48616375.mp3")
 
-timeLine.add(morgen)
+timeLine.add(koktail)
+timeLine.add(golden_fishes)
+timeLine.add(without_you)
+timeLine.add(meteor_rain)
 
-timeLine.slice(1, 10000)
+timeLine.crop(1, 28000, 58000)
+timeLine.crop(2, 32500, 45000)
+timeLine.crop(3, 58000, 110000)
+timeLine.crop(4, 53000, 78000)
 
-timeLine.remove(3)
+timeLine.fade_out(1, 3000)
+timeLine.fade_in(2, 3000)
+timeLine.fade_out(2, 3000)
+timeLine.fade_in(3, 3000)
+timeLine.fade_out(3, 3000)
+timeLine.fade_in(4, 3000)
 
-timeLine.undo()
-timeLine.undo()
-
-timeLine.render("ahahah.mp3", "mp3")
-
+timeLine.render("LSP1Love.mp3", "mp3")
