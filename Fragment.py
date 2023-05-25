5b9f4a5386ebbf32082afca529d6665c819e34b6
VartoSss <ngaseneger@gmail.com>
2023-05-25 08:01:27 +0500

    Cleaned Fragment class

--- Fragment.py
+++ Fragment.py
@@ -9,15 +9,6 @@ Fragment = TypeVar('Fragment', bound='Fragment')
 class Fragment:
     __last_id = 1
 
-    def __init__(self, path_to_audio: str) -> None:
-        self.path_to_audio = path_to_audio
-        self.set_value_by_path(path_to_audio)
-        self.set_name_by_path(path_to_audio)
-        self.id = self.__last_id
-        self.next = None
-        self.previous = None
-        Fragment.__last_id += 1
-
     def __init__(self, path_to_audio, name=None, value=None, id=None, previous=None, next=None):
         if name is None:
             self.path_to_audio = path_to_audio
@@ -49,33 +40,6 @@ class Fragment:
     def set_name_by_path(self, path_to_audio: str):
         self.name = os.path.splitext(os.path.basename(path_to_audio))[0]
 
-    def slice(self, from_miliseconds: int, to_miliseconds: int) -> None:
-        self.value = self.value[from_miliseconds:to_miliseconds]
-
-    def change_speed(self, speed_multiplier: float) -> None:
-        sound_with_altered_frame_rate = self.value._spawn(
-            self.value.raw_data, overrides={
-                "frame_rate": int(self.value.frame_rate * speed_multiplier)
-            })
-        self.value = sound_with_altered_frame_rate.set_frame_rate(
-            self.value.frame_rate)
-
-    def change_volume(self, volume_delta_decibels: int) -> None:
-        self.value += volume_delta_decibels
-        self.value = self.value.apply_gain(volume_delta_decibels)
-
-    def cuncat_with(self, other_fragment: Fragment) -> None:
-        self.value += other_fragment.value
-
-    def fade_out(self, duration_miliseconds):
-        self.value = self.value.fade_out(duration_miliseconds)
-
-    def fade_in(self, duration_miliseconds):
-        self.value = self.value.fade_in(duration_miliseconds)
-
     def copy_value(self):
         new_value = deepcopy(self.value)
         return new_value
-
-    def export_fragment(self, path_with_name: str, format_file: str) -> None:
-        self.value.export(path_with_name, format=format_file)
